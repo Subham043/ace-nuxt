42735570ea801c5703fec3d07a3f427c042893a5
ajayjury <ajay@jurysoft.com>
2022-03-09 14:09:51 +0530

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,78 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'ace',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'vue-slick-carousel/dist/vue-slick-carousel.css',
+    '~/static/assets/css/bootstrap.min.css',
+    '~/static/assets/css/styles.css',
+    '~/static/assets/css/style.css',
+    '~/static/assets/css/responsive.css',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [{ src: '~/plugins/vue-slick-carousel.js' }],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/stylelint
+    // '@nuxtjs/stylelint-module',
+    '@nuxtjs/fontawesome'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en',
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+
+  loading: {
+    color: '#02b8f4',
+    height: '3px',
+    throttle: 0
+  },
+
+  fontawesome: {
+    icons: {
+      solid: true,
+      brands: true,
+      regular: true,
+    }
+  }
+
+}
---
+++ pages/product-catalogue/index.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="App">
+    <BreadCrumb page-name="Product Catalogue" />
+    <div class="wpo-protfolio-area section-padding">
+      <div class="container">
+        <div class="row">
+          <div class="col col-xs-12 sortable-gallery">
+            <div class="gallery-container gallery-fancybox masonry-gallery row">
+              <div class="col-lg-6 col-md-6 col-12 custom-grid">
+                <div class="">
+                  <div class="wpo-protfolio-single">
+                    <div class="wpo-protfolio-img">
+                      <img src="/assets/images/img-2.435a2cec.jpg" alt="" />
+                    </div>
+                    <div class="wpo-protfolio-text">
+                      <h2>Minimalism</h2>
+                      <span>Illustration . Art Direction</span
+                      ><NuxtLink to="/product">View Work</NuxtLink>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-6 col-12 custom-grid">
+                <div class="">
+                  <div class="wpo-protfolio-single">
+                    <div class="wpo-protfolio-img">
+                      <img src="/assets/images/img-6.3e4c39f4.jpg" alt="" />
+                    </div>
+                    <div class="wpo-protfolio-text">
+                      <h2>Abstract Art</h2>
+                      <span>Illustration . Art Direction</span
+                      ><NuxtLink to="/product">View Work</NuxtLink>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-6 col-12 custom-grid">
+                <div class="">
+                  <div class="wpo-protfolio-single">
+                    <div class="wpo-protfolio-img">
+                      <img src="/assets/images/img-7.77703f45.jpg" alt="" />
+                    </div>
+                    <div class="wpo-protfolio-text">
+                      <h2>3D Project</h2>
+                      <span>Illustration . Art Direction</span
+                      ><NuxtLink to="/product">View Work</NuxtLink>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-6 col-12 custom-grid">
+                <div class="">
+                  <div class="wpo-protfolio-single">
+                    <div class="wpo-protfolio-img">
+                      <img src="/assets/images/img-8.b596fadf.jpg" alt="" />
+                    </div>
+                    <div class="wpo-protfolio-text">
+                      <h2>Product Design</h2>
+                      <span>Illustration . Art Direction</span
+                      ><NuxtLink to="/product">View Work</NuxtLink>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-6 col-12 custom-grid">
+                <div class="">
+                  <div class="wpo-protfolio-single">
+                    <div class="wpo-protfolio-img">
+                      <img src="/assets/images/img-5.f1fe8622.jpg" alt="" />
+                    </div>
+                    <div class="wpo-protfolio-text">
+                      <h2>Web Design</h2>
+                      <span>Illustration . Art Direction</span
+                      ><NuxtLink to="/product">View Work</NuxtLink>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-6 col-12 custom-grid">
+                <div class="">
+                  <div class="wpo-protfolio-single">
+                    <div class="wpo-protfolio-img">
+                      <img src="/assets/images/img-4.3318167a.jpg" alt="" />
+                    </div>
+                    <div class="wpo-protfolio-text">
+                      <h2>Modern BG</h2>
+                      <span>Illustration . Art Direction</span
+                      ><NuxtLink to="/product">View Work</NuxtLink>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'ProductCataloguePage',
+}
+</script>
